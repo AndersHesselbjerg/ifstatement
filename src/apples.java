ed95caaaa60e0727ff878f3b424abac5ffb257dc
ande8548@stud.kea.dk <anders_h91@hotmail.com>
2021-03-09 13:25:29 +0100

    Initial commit

---
+++ src/apples.java
@@ -0,0 +1,28 @@
+
+public class apples {
+    public static void main(String args[]){
+        int test = 9;
+
+        if (test == 9) {
+            System.out.println("yes");
+
+        }else {
+            System.out.println("this is else");
+        }
+    }
+
+}
+
+
+//  int x = 5;
+//        int y = 10;
+//
+//        if (x>y){
+//            System.out.println("y er større end x");
+//        }
+//        else System.out.println("y er mindre en x");
+
+
+
+// () paranteser er en condition/test
+// == betyder om værdien den samme. = betyder at du giver denne værdi. != betyder at den ikke er den samme værdi
